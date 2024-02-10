64397c353a5953bf6c4aa694a5fcd1ec967786e6
AlexandreGoular <xandeaugustogoulart@gmail.com>
2024-02-10 19:55:14 -0300

    exercicio 3

---
+++ aulas/tiposPrimitivos.py
@@ -0,0 +1,9 @@
+#Entrada
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+
+#Processamento
+soma = n1 + n2
+
+#Saida
+print("A soma {} + {} = {}".format(n1, n2, soma))
---
+++ exercicios/ex03.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia dois numeros e mostre a soma entre eles
+
+# Entrada
+numeroUm = int(input("Digite o primeiro numero: "))
+numeroDois = int(input("Digite o segundo numero: "))
+
+# Processamento
+somaNumeros = numeroUm + numeroDois
+
+# Saida
+print("{} + {} = {}".format(numeroUm, numeroDois, somaNumeros))
