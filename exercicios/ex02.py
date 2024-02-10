bdca6aaa123d633268b3a757a11b4b78e66a179b
AlexandreGoular <xandeaugustogoulart@gmail.com>
2024-02-10 19:39:52 -0300

    exercicio 2

---
+++ exercicios/ex02.py
@@ -0,0 +1,5 @@
+# Crie um programa que tenha como entrada seu nome e
+# retorne 'é um prazer te conhecer, "nome"'
+
+nome = str(input("Digite seu Nome: "))
+print("É um prazer te conhecer, {}".format(nome))
